4d7e6c7363a765be54cc8d01c15bd2216ef602d2
asabando <70186052+asabando@users.noreply.github.com>
2020-10-09 13:37:38 +0800

    Create script.js

---
+++ act52/js/script.js
@@ -0,0 +1,47 @@
+ function registrationform() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "" || x == null) {
+      alert("First Name must be filled out.");
+      return false;
+    }
+  }
+
+  function registrationform() {
+    var x = document.forms["myForm"]["uname"].value;
+    if (x == "" || x == null) {
+      alert("Username must be filled out.");
+      return false;
+    }
+  }
+
+  function registrationform() {
+    var x = document.forms["myForm"]["password"].value;
+    if (x == "" || x == null) {
+      alert("Password must be filled out max. of 8.");
+      return false;
+    }
+  }
+
+  function registrationform() {
+    var x = document.forms["myForm"]["cpassword"].value;
+    if (x == "" || x == null) {
+      alert("Confirm Password must be filled out.");
+      return false;
+    }
+  }
+
+  function registrationform() {
+    var x = document.forms["myForm"]["birthday"].value;
+    if (x == "" || x == null) {
+      alert("Birthday must be filled out.");
+      return false;
+    }
+  }
+
+  function registrationform() {
+    var x = document.forms["myForm"]["cbxPolicy1"].value;
+    if (x == "" || x == null) {
+      alert("Terms & Condition must be check.");
+      return false;
+    }
+  }
